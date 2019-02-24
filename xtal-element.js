4d71cbcf4800e754b8a83edb9a2e9acae7af29aa
bahrus <anderson.bruce.b@gmail.com>
2019-02-24 07:37:01 -0500

    ...

--- xtal-element.js
+++ xtal-element.js
@@ -26,6 +26,28 @@ export class XtalElement extends XtallatX(HTMLElement) {
     onPropsChange() {
         if (this._disabled || !this._connected || !this.ready)
             return false;
+        const rc = this.renderContext;
+        const esc = this.eventContext;
+        if (this.mainTemplate !== undefined) {
+            if (esc && esc.eventManager !== undefined) {
+                if (!this._initialized) {
+                    esc.eventManager(this.root, esc);
+                }
+            }
+            if (rc && rc.init !== undefined) {
+                if (this._initialized) {
+                    rc.update(rc, this.root);
+                }
+                else {
+                    rc.init(this.mainTemplate, rc, this.root, this.renderOptions);
+                    //rc.update = this.update;
+                }
+            }
+            else {
+                this.root.appendChild(this.mainTemplate.content.cloneNode(true));
+            }
+            this._initialized = true;
+        }
         return true;
     }
 }
