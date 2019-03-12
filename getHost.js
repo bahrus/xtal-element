df87d7c6220d1235b42567cd817927bb8ddeefbf
bahrus <anderson.bruce.b@gmail.com>
2019-03-12 12:13:09 -0400

    ...

--- getHost.js
+++ getHost.js
@@ -4,6 +4,9 @@ export function getHost(el) {
         if (parent.nodeType === 11) {
             return parent['host'];
         }
+        else if (parent.tagName.indexOf('-') > -1) {
+            return parent;
+        }
         else if (parent.tagName === 'BODY') {
             return null;
         }
--- getHost.ts
+++ getHost.ts
@@ -3,7 +3,9 @@ export function getHost(el: HTMLElement) : HTMLElement | null {
     while (parent = (parent.parentNode)) {
         if (parent.nodeType === 11) {
             return (<any>parent)['host'] as HTMLElement;
-        } else if (parent.tagName === 'BODY') {
+        } else if ((<HTMLElement>parent).tagName.indexOf('-') > -1) {
+            return parent;
+        }  else if (parent.tagName === 'BODY') {
             return null;
         }
     }
