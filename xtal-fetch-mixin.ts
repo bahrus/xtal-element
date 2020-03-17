2553bc8973f46489794bf44518a6e34e298fec62
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-03-17 07:36:46 -0400

    ...

--- xtal-fetch-mixin.ts
+++ xtal-fetch-mixin.ts
@@ -1,6 +1,8 @@
 import { XtalViewElement } from "./xtal-view-element";
 
 type Constructor<T = {}> = new (...args: any[]) => T;
-
+const href = 'href';
+const req_init = 'req-init';
 export function XtalFetchMixin<TView, TBase extends Constructor<XtalViewElement<TView>>>(superClass: TBase) {
+    
 }
