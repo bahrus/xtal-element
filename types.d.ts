c90e7d68ed0a0c38d68e2dd0cfc882b743361295
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-05-12 18:57:53 -0400

    ...

---
+++ types.d.ts
@@ -0,0 +1,18 @@
+// export type Primitives = String | Number | Boolean | Symbol | Object;
+// export type PropDefGet<T> = (t: T) => PropDef;
+// export interface PropDef{
+//     type: Primitives;
+//     readOnly: boolean;
+//     reflect: boolean;
+//     attribute: boolean;
+//     notify: boolean;
+// }
+
+export interface AttributePartition{
+
+}
+
+export interface PropPartition{
+
+}
+
