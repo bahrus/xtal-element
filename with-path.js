ce082db1fff56b7afc7f5b08f9d7f13c8cc6ba24
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-05-28 20:33:32 -0400

    ...

--- with-path.js
+++ with-path.js
@@ -6,23 +6,9 @@ export const with_path = 'with-path';
  */
 export function WithPath(superClass) {
     return class extends superClass {
-        get withPath() {
-            return this._withPath;
-        }
-        /**
-        * object inside a new empty object, with key equal to this value.
-        * E.g. if the incoming object is {foo: 'hello', bar: 'world'}
-        * and with-path = 'myPath'
-        * then the source object which be merged into is:
-        * {myPath: {foo: 'hello', bar: 'world'}}
-        * @attr with-path
-        */
-        set withPath(val) {
-            this.setAttribute(with_path, val);
-        }
         wrap(obj, target = {}) {
-            if (this._withPath) {
-                createNestedProp(target, this._withPath.split('.'), obj, true);
+            if (this.withPath) {
+                createNestedProp(target, this.withPath.split('.'), obj, true);
                 return target;
             }
             else {
--- with-path.ts
+++ with-path.ts
@@ -10,11 +10,6 @@ export const with_path = 'with-path';
  */
 export function WithPath<TBase extends Constructor<HTMLElement>>(superClass: TBase) {
     return class extends superClass {
-        _withPath!: string;
-
-        get withPath() {
-            return this._withPath;
-        }
         /**
         * object inside a new empty object, with key equal to this value.
         * E.g. if the incoming object is {foo: 'hello', bar: 'world'}
@@ -23,13 +18,11 @@ export function WithPath<TBase extends Constructor<HTMLElement>>(superClass: TBa
         * {myPath: {foo: 'hello', bar: 'world'}}
         * @attr with-path
         */
-        set withPath(val) {
-            this.setAttribute(with_path, val);
-        }
+        withPath!: string;
 
         wrap(obj: any, target: object = {}){
-            if (this._withPath) {
-                createNestedProp(target, this._withPath.split('.'), obj, true);
+            if (this.withPath) {
+                createNestedProp(target, this.withPath.split('.'), obj, true);
                 return target;
             }else{
                 return obj;
