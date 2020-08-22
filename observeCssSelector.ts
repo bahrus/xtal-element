31ad215d275a93b00891f376d2d75c4cda1ba158
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-08-22 13:02:03 -0400

    ...

--- observeCssSelector.ts
+++ observeCssSelector.ts
@@ -8,50 +8,54 @@ declare global{
     }
 }
 
+const eventNames = ["animationstart", "MSAnimationStart", "webkitAnimationStart"];
+
+export function addCSSListener(id: string, self: any, targetSelector: string, insertListener: any, customStyles: string = ''){
+        // See https://davidwalsh.name/detect-node-insertion
+        if(self._boundInsertListener) return;
+        const styleInner = /* css */`
+        @keyframes ${id} {
+            from {
+                opacity: 0.99;
+            }
+            to {
+                opacity: 1;
+            }
+        }
+
+        ${targetSelector}{
+            animation-duration: 0.001s;
+            animation-name: ${id};
+        }
+        ${customStyles}`;
+        const style = document.createElement('style');
+        style.innerHTML = styleInner;
+        self._host = self.getRootNode();//experimental  <any>getShadowContainer((<any>this as HTMLElement));
+        if(self._host.nodeType === 9){
+            self._host = document.firstElementChild;
+        }
+        const hostIsShadow = self._host.localName !== 'html';
+        if(hostIsShadow){
+            self._host.appendChild(style);
+        }else{
+            document.head.appendChild(style);
+        }
+        self._boundInsertListener = insertListener.bind(self);
+        const container = hostIsShadow ? self._host : document;
+        eventNames.forEach(name =>{
+            container.addEventListener(name, self._boundInsertListener, false);
+        })
+}
+
 
 export function observeCssSelector<TBase extends Constructor<HTMLElement>>(superClass: TBase) {
-
-    const eventNames = ["animationstart", "MSAnimationStart", "webkitAnimationStart"];
 
     return class extends superClass {
 
         _boundInsertListener!: any;
         _host: any;
         addCSSListener(id: string, targetSelector: string, insertListener: any, customStyles: string = ''){
-            // See https://davidwalsh.name/detect-node-insertion
-            if(this._boundInsertListener) return;
-            const styleInner = /* css */`
-            @keyframes ${id} {
-                from {
-                    opacity: 0.99;
-                }
-                to {
-                    opacity: 1;
-                }
-            }
-    
-            ${targetSelector}{
-                animation-duration: 0.001s;
-                animation-name: ${id};
-            }
-            ${customStyles}`;
-            const style = document.createElement('style');
-            style.innerHTML = styleInner;
-            this._host = this.getRootNode();//experimental  <any>getShadowContainer((<any>this as HTMLElement));
-            if(this._host.nodeType === 9){
-                this._host = document.firstElementChild;
-            }
-            const hostIsShadow = this._host.localName !== 'html';
-            if(hostIsShadow){
-                this._host.appendChild(style);
-            }else{
-                document.head.appendChild(style);
-            }
-            this._boundInsertListener = insertListener.bind(this);
-            const container = hostIsShadow ? this._host : document;
-            eventNames.forEach(name =>{
-                container.addEventListener(name, this._boundInsertListener, false);
-            })
+           addCSSListener(id, this, targetSelector, insertListener, customStyles);
         }
         disconnect(){
             if(this._boundInsertListener){
