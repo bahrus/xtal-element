f92355e130ed80d7810cbdd259e44522aa5db114
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-12-24 15:51:12 -0500

    Refactor

---
+++ lib/propUp.ts
@@ -0,0 +1,18 @@
+/**
+ * Needed for asynchronous loading
+ * @param props Array of property names to "upgrade", without losing value set while element was Unknown
+ * @param defaultValues:   If property value not set, set it from the defaultValues lookup
+ * @private
+ */
+export function propUp<T = any>(self: HTMLElement, props: string[], defaultValues?: T){
+    props.forEach(prop => {
+        let value = (<any>self)[prop];
+        if(value === undefined && defaultValues !== undefined){
+            value = (<any>defaultValues)[prop];
+        }
+        if (self.hasOwnProperty(prop)) {
+            delete (<any>self)[prop];
+        }
+        if(value !== undefined) (<any>self)[prop] = value;
+    });
+}
