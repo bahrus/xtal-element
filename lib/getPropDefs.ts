52020757d13bace39664cbc8ca35aeb2722898e6
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-12-25 11:08:39 -0500

    ...

--- lib/getPropDefs.ts
+++ lib/getPropDefs.ts
@@ -1,9 +1,9 @@
 import {destructPropInfo, PropDef, PropDefGet} from '../types.d.js';
 import {getDestructArgs} from './getDestructArgs.js';
 
-export function getPropDefs(proppers: destructPropInfo[]): PropDef[]{
+export function getPropDefs(propDefGetters: destructPropInfo[]): PropDef[]{
     const returnObj : PropDef[] = [];
-    for(const propper of proppers){
+    for(const propper of propDefGetters){
         const args = getDestructArgs(propper);
         const propDef = propper(0);
         for(var arg of args){
