6d062a3e82c19d04c6d8963ad9a914b6089dcca8
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-12-24 20:24:22 -0500

    ...

---
+++ lib/define.ts
@@ -0,0 +1,20 @@
+export function define(MyElementClass: any){
+    const tagName = MyElementClass.is as string;
+    let n = 0;
+    let name = tagName;
+    while(true){
+        if(n > 0) name = `${tagName}-${n}`;
+        const test = customElements.get(name);
+        if(test === undefined){
+            MyElementClass.isReally = name;
+            customElements.define(name, MyElementClass);
+            return;
+        }else{
+            if(test === MyElementClass){
+                MyElementClass.isReally = name;
+                return;
+            }
+        }
+        n++;
+    }
+}
