39b60a7ba1a05b91559ec5935627fa5cf2e0f524
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-01-09 15:35:28 -0500

    ...

--- lib/getProp.ts
+++
@@ -1,8 +0,0 @@
-export function getProp(val: any, pathTokens: string[]){
-    let context = val;
-    for(const token of pathTokens){
-      context = context[token];
-      if(context === undefined) break;
-    }
-    return context;
-}
