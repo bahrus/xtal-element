e3e097d8e3052d883f4c6e9095228110f8de5dd2
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-02-27 20:57:36 -0500

    ...

--- lib/DOMKeyPE.ts
+++ lib/DOMKeyPE.ts
@@ -4,8 +4,25 @@ import {PEUnionSettings} from 'trans-render/lib/types.d.js';
 
 export class DOMKeyPE{
     constructor(public domCacheProp: string = 'domCache'){}
-    apply(surface: HTMLElement, el: HTMLElement, ref: any){
-        applyPE(surface, el, ref as PEUnionSettings);
+    checkForDOMSubstitution(surface: HTMLElement, el: HTMLElement, val: any, key: symbol){
+        if(typeof(val) === 'symbol' && val.description !== undefined){
+            const newEl = document.createElement(val.description);
+            el.insertAdjacentElement('afterend', newEl);
+            const cache = (<any>surface)[this.domCacheProp];
+            const pinned = cache[key];
+            if(Array.isArray(pinned)){
+                const idx = pinned.findIndex(test => test === el);
+                pinned[idx] = newEl;
+            }else{
+                cache[key] = newEl;
+            }
+            el.remove();
+        }else{
+            this.apply(surface, el, val);
+        }
+    }
+    apply(surface: HTMLElement, el: HTMLElement, val: any){
+        applyPE(surface, el, val as PEUnionSettings);
     }
     do(refs: any[], dependencies: string[], reactor: RxSuppl){
         const surface = reactor.surface;
@@ -20,30 +37,33 @@ export class DOMKeyPE{
                 for(const key of refKeys){
                     let val = ref[key];
                     if(val === undefined) continue;
+                    const cacheKey = cache[key];
                     switch(typeof val){
                         case 'string':
                             if(val==='"' && lastNonDittoVal !== undefined){
                                 val = lastNonDittoVal;
                             }else{
-                                (cache[key] as HTMLElement).textContent = val;
+                                (cacheKey as HTMLElement).textContent = val;
                                 continue;
                             }
                             break;
                         case 'number':
-                            (cache[key] as HTMLElement).textContent = val.toString();
+                            (cacheKey as HTMLElement).textContent = val.toString();
                             continue;
                             break;
                         case 'object':
                             lastNonDittoVal = val;
                             break;
                     }
-                    const matchOrMatches = cache[key];
+                    const matchOrMatches = cacheKey;
                     if(Array.isArray(matchOrMatches)){
                         for(var el of matchOrMatches){
-                            this.apply(surface as HTMLElement, el as HTMLElement, lastNonDittoVal);
+                            this.checkForDOMSubstitution(surface as HTMLElement, el, lastNonDittoVal, key)
+                            //this.apply(surface as HTMLElement, el as HTMLElement, lastNonDittoVal);
                         }
                     }else{
-                        this.apply(surface as HTMLElement, matchOrMatches as HTMLElement, lastNonDittoVal);
+                        this.checkForDOMSubstitution(surface as HTMLElement, matchOrMatches as HTMLElement, lastNonDittoVal, key);
+                        //this.apply(surface as HTMLElement, matchOrMatches as HTMLElement, lastNonDittoVal);
                     }
                 }
             }
