233d68fc81bc14fd523d617e8d6421fc489d2826
bahrus <anderson.bruce.b@gmail.com>
2021-04-18 20:36:45 -0400

    ...

--- lib/XtalCore.ts
+++ lib/XtalCore.ts
@@ -4,7 +4,8 @@ import {getSlicedPropDefs} from './getSlicedPropDefs.js';
 import {letThereBeProps} from './letThereBeProps.js';
 import {hydrate} from './hydrate.js';
 import {Rx, ReactiveSurface} from './Rx.js';
+import {initInternals} from './initInternals.js';
 
 export const xc = {
-    define, getSlicedPropDefs, letThereBeProps, hydrate, Rx
+    define, getSlicedPropDefs, letThereBeProps, hydrate, Rx, initInternals
 };
