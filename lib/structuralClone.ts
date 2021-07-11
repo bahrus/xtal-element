ac024548a8ed91c045415513777a4c2602e2df18
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-10 22:19:31 -0400

    ...

--- lib/structuralClone.ts
+++ lib/structuralClone.ts
@@ -1,9 +1,7 @@
 //https://surma.dev/things/deep-copy/index.html
-
+//using history api, hitting upper limit of throttling:
+//structuralClone.js:4 Throttling navigation to prevent the browser from hanging. See https://go.microsoft.com/fwlink/?linkid=2048109. Command line switch --disable-ipc-flooding-protection can be used to bypass the protection
+//
 export function structuralClone(obj: any) {
-    const oldState = history.state;
-    history.replaceState(obj, document.title);
-    const copy = history.state;
-    history.replaceState(oldState, document.title);
-    return copy;
+    return JSON.parse(JSON.stringify(obj));
 }
