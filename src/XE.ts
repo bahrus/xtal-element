5f205b85c52ba331982c9785dde34569447af6e7
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-08-22 09:51:15 -0400

    ...

--- src/XE.ts
+++ src/XE.ts
@@ -1,5 +1,6 @@
-import {CE} from 'trans-render/lib/CE.js';
+import {CE, PropInfo} from 'trans-render/lib/CE.js';
+import {XAction} from './types.js';
 
-export class XE extends CE{
+export class XE<MCProps = any, MCActions = MCProps, TPropInfo = PropInfo, TAction extends XAction<MCProps> = XAction<MCProps>> extends CE<MCProps, MCActions, TPropInfo, TAction>{
     
 } 
---
+++ src/types.ts
@@ -0,0 +1,17 @@
+import {Action, LogicOpProp} from 'trans-render/lib/types.js';
+
+export interface XAction<MCProps = any> extends Action<MCProps>{
+
+     andAllOf?: LogicOpProp<MCProps>,
+     orAllOf?: LogicOpProp<MCProps>,
+     ifAnyOf?: LogicOpProp<MCProps>,
+
+     atLeastOneOf?: LogicOpProp<MCProps>,
+     orAtLeastOneOf?: LogicOpProp<MCProps>
+     ifNot?: LogicOpProp<MCProps>,
+     andIfNot?: LogicOpProp<MCProps>,
+     orIfNot?: LogicOpProp<MCProps>,
+     ifEquals?: LogicOpProp<MCProps>,
+     andIfEquals?: LogicOpProp<MCProps>,
+     orIfEquals?: LogicOpProp<MCProps>,
+}
