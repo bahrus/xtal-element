f99300866057c4c57e1ff67f0723135d8812e445
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-08-22 13:58:13 -0400

    ...

--- src/XE.ts
+++ src/XE.ts
@@ -9,16 +9,20 @@ export class XE<MCProps = any, MCActions = MCProps, TPropInfo = PropInfo, TActio
         for(const logicalOp in expr){
             const rhs: any = (<any>expr)[logicalOp];
             
-            if(logicalOp.endsWith('OneOf')) {
+            if(logicalOp.endsWith('LeastOneOf')) {
                 ctx.op = 'or';
             }else if(logicalOp.endsWith('AllOf')){
                 ctx.op = 'and';
+            }else if(logicalOp.endsWith('NoneOf')){
+                ctx.op = 'nor';
+            }else if(logicalOp.endsWith('Equals')){
+                ctx.op = 'eq';
             }else{
                 continue;
             }
             
             if(!Array.isArray(rhs)) throw 'NI'; //Not Implemented
-            const subAnswer = self.pqs(self, rhs, src)
+            const subAnswer = self.pqs(self, rhs, src, ctx);
             switch(op){
                 case 'and':
                     if(!subAnswer) return false;
@@ -31,7 +35,52 @@ export class XE<MCProps = any, MCActions = MCProps, TPropInfo = PropInfo, TActio
         return answer;
     }
 
-    override pqs(self: this, expr: ListOfLogicalExpressions, src: MCProps, ctx: LogicEvalContext = {op: 'and'}) : boolean {
+    override pqs(self: this, expr: ListOfLogicalExpressions, src: MCProps, ctx: LogicEvalContext) : boolean {
+        const {op} = ctx;
+        let answer: boolean = false;
+        switch(op){
+            case 'nor':
+            case 'and':
+                answer = true;
+                break;
+            
+        }
+        let lastVal: any;
+        let isFirst = true;
+        for (const subExpr of expr) {
+            const subAnswer = self.pqsv(self, src, subExpr, ctx);
+            switch(op){
+                case 'and':
+                    if(!subAnswer) return false;
+                    break;
+                case 'or':
+                    if(subAnswer) return true;
+                    break;
+                case 'nor':
+                    if(subAnswer) return false;
+                    break;
+                case 'nand':
+                    if(!subAnswer) return true;
+                    break;
+                case 'eq':
+                    if(isFirst){
+                        lastVal = subA
+                    }
+            }
+        }
+        return answer;    
+    }
 
+    override pqsv(self: this, src: any, subExpr: string | number | symbol | LogicOp<any>, ctx: LogicEvalContext): boolean{
+        switch(typeof subExpr){
+            case 'string':
+                if(ctx.op === 'eq') return src[subExpr];
+                return !!src[subExpr];
+            case 'object':
+                return self.pq(self, subExpr, src, ctx);
+                break;
+            default:
+                throw 'NI'; //Not Implemented
+        }
     }
 } 
--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,19 @@
-import {Action, LogicOpProp} from 'trans-render/lib/types.js';
+import {Action, LogicOp, LogicOpProp} from 'trans-render/lib/types.js';
 
 export interface XAction<MCProps = any> extends Action<MCProps>{
 
      andAllOf?: LogicOpProp<MCProps>,
      orAllOf?: LogicOpProp<MCProps>,
-     ifAnyOf?: LogicOpProp<MCProps>,
 
+
+     ifAtLeastOneOf?: LogicOpProp<MCProps>
      atLeastOneOf?: LogicOpProp<MCProps>,
-     orAtLeastOneOf?: LogicOpProp<MCProps>
-     ifNot?: LogicOpProp<MCProps>,
-     andIfNot?: LogicOpProp<MCProps>,
-     orIfNot?: LogicOpProp<MCProps>,
+     orAtLeastOneOf?: LogicOpProp<MCProps>,
+
+     ifNoneOf?: LogicOpProp<MCProps>,
+     andNoneOf?: LogicOpProp<MCProps>,
+     orNoneOf?: LogicOpProp<MCProps>,
+     
      ifEquals?: LogicOpProp<MCProps>,
      andIfEquals?: LogicOpProp<MCProps>,
      orIfEquals?: LogicOpProp<MCProps>,
