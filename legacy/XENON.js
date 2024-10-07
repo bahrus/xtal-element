1ef6b7d7de7376f2cc961a355b4a65edb1bcdfe6
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-07 06:23:23 -0400

    0.0.617

--- legacy/XENON.js
+++
@@ -1,9 +0,0 @@
-export class XENON {
-    static async define(imp) {
-        const config = (await imp()).default;
-        config.mixins = config.mixins || [];
-        const { XE } = await import('./XE.js');
-        const xe = new XE(config);
-        return xe.custElClass;
-    }
-}
