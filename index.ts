a6eefa14019e5eb6048f12fd7b46b8f533e169af
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-27 16:34:08 -0500

    0.0.626

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import {def} from 'trans-render/lib/def.js';
 import {XtalElement} from './xtal-element.js';
 export {XtalElement} from './xtal-element.js';
+export {Mount} from 'trans-render/Mount.js';
 
 await XtalElement.bootUp();
 def('xtal-element', XtalElement);
